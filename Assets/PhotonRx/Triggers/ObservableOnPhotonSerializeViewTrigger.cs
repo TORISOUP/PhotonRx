1a2b166e660068b0ea856854089f645c0d91fa0b
TORISOUP <tori.birdstrike@gmail.com>
2018-05-16 00:29:50 +0900

    Fix ObservableOnPhotonSerializeViewTrigger

--- Assets/PhotonRx/Triggers/ObservableOnPhotonSerializeViewTrigger.cs
+++ Assets/PhotonRx/Triggers/ObservableOnPhotonSerializeViewTrigger.cs
@@ -8,6 +8,8 @@ namespace PhotonRx.Triggers
     [DisallowMultipleComponent]
     public class ObservableOnPhotonSerializeViewTrigger : ObservableTriggerBase
     {
+        private bool isInitalized;
+
         private Subject<Tuple<PhotonStream, PhotonMessageInfo>> onPhotonSerializeView;
 
         private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -23,6 +25,14 @@ namespace PhotonRx.Triggers
         /// </summary>
         public IObservable<Tuple<PhotonStream, PhotonMessageInfo>> OnPhotonSerializeViewAsObservable()
         {
+            if (!isInitalized)
+            {
+                var view = PhotonView.Get(this);
+                if (view == null) throw new Exception("Not found PhotonView.");
+                if (!view.ObservedComponents.Contains(this)) view.ObservedComponents.Add(this);
+                isInitalized = true;
+            }
+
             return onPhotonSerializeView ??
                    (onPhotonSerializeView = new Subject<Tuple<PhotonStream, PhotonMessageInfo>>());
         }
